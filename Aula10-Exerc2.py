2c44efae92db1604dd9f07f72fa55bfbbd6d3b6f
David Sotto Mayor <ddsmmf@gmail.com>
2021-05-17 22:04:43 -0400

    Aula11-Listas-Desafio

--- Aula10-Exerc2.py
+++
@@ -1,8 +0,0 @@
-#! python
-sexo = ''
-
-while(sexo != 'M' and sexo != 'F'):
-    sexo = input(
-        'Digite se seu sexo é Masculino(M) ou Feminino(F): ').upper()
-
-print('Ok, agora sabemos seu sexo!')
