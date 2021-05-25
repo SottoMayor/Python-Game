4bbff1836eb123e5bac59d32b33af06c07a6f652
David Sotto Mayor <ddsmmf@gmail.com>
2021-05-24 22:29:33 -0400

    introdução à POO

---
+++ main.py
@@ -0,0 +1,8 @@
+from pokemon import Pokemon
+
+pikachu = Pokemon('Pikachu', 'Fogo', 100, 75)
+
+charmander = Pokemon('Charmander', 'Água', 75, 100)
+
+print(pikachu.nome)
+print(charmander.tipo)
---
+++ pokemon.py
@@ -0,0 +1,7 @@
+# POO - Vamos construir nossa classe de Pokemón!
+class Pokemon:
+    def __init__(self, nome, tipo, ataque, defesa):
+        self.nome = nome
+        self.tipo = tipo
+        self.ataque = ataque
+        self.defesa = defesa
